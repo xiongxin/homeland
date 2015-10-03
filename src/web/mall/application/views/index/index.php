01b9063193de11534d44a82607845a89924b187a
xuebingwang <406964108@qq.com>
2015-10-03 13:50:28 +0800

    js 调用微信扫一扫

--- src/web/mall/application/views/index/index.php
+++ src/web/mall/application/views/index/index.php
@@ -42,7 +42,18 @@
 <block name="script">
 <script type="text/javascript" src="/v2/js/TouchSlide.1.1.js"></script>
 <script type="text/javascript">
+function club_sign(){
+    wx.scanQRCode({
+        needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        success: function (res) {
+            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+            alert(result);
+        }
+    });
+}
 $(document).ready(function(){
+
     $('embed').css({width:$(".container").width()+30,height:'auto'});
 
     $("#slide img").each(function(){
