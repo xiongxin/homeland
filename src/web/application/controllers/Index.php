ccb9293669477993dc4821d92440963a72b34984
xuebingwang <406964108@qq.com>
2016-03-28 23:32:40 +0800

    签到功能完成

--- src/web/application/controllers/Index.php
+++ src/web/application/controllers/Index.php
@@ -16,11 +16,48 @@ class IndexController extends Mall {
     }
 
     public function signAction(){
+        if(IS_POST){
+            $enroll_id = intval(I('enroll_id'));
 
+            if(empty(I('enroll_id'))){
+                $this->error('参数错误，报名ID不能为空！');
+            }
+            if(M('t_enroll')->update(['is_sign'=>Model::BOOL_YES,'sign_time'=>time_format()],['AND'=>['id'=>$enroll_id,'is_affirm'=>Model::BOOL_YES]])){
+                $this->success('签到成功！');
+            }else{
+                $this->error('签到失败，请重新再试或联系管理人员！');
+            }
+        }
     }
 
     public function searchAction(){
 
-        $this->success('1111');
+        $keyword = I('keyword');
+        if(empty($keyword)){
+            $this->error('请输入手机号码！');
+        }
+
+        $list = M('t_enroll(a)')->select(
+            [
+                '[><]t_company_reg(b)'=>['a.id'=>'eid'],
+            ],
+            [
+                'a.id',
+                'a.mobile',
+                'a.create_time',
+                'b.chairman_name',
+                'b.sex',
+                'b.company_name',
+            ],
+            [
+                'AND'=>[
+                    'a.is_affirm'=>Model::BOOL_YES,
+                    'a.is_sign'=>Model::BOOL_NO,
+                    'a.mobile[~]'=>$keyword,
+                ]
+            ]);
+
+//        SeasLog::info(M()->last_query());
+        $this->success('','',['list'=>$list]);
     }
 }
--- src/web/application/views/index/sign.php
+++ src/web/application/views/index/sign.php
@@ -1,16 +1,18 @@
 <div class="page">
     <div class="hd">
         <div class="bd spacing">
-            <a href="javascript:;" class="weui_btn weui_btn_primary">点击扫码签到</a>
+            <div class="button_sp_area">
+                <a href="javascript:;" class="weui_btn weui_btn_warn">点击扫码签到</a>
+            </div>
         </div>
     </div>
-    <div class="bd" id="js-container">
+    <div class="bd">
         <!--<a href="javascript:;" class="weui_btn weui_btn_primary">点击展现searchBar</a>-->
         <div class="weui_search_bar" id="search_bar">
-            <form action="<?U('/index/search')?>" class="weui_search_outer ajax-form" method="post" success="search_success">
+            <form action="<?=U('/index/search')?>" class="weui_search_outer ajax-form" success="search_success">
                 <div class="weui_search_inner">
                     <i class="weui_icon_search"></i>
-                    <input type="search" pattern="[0-9]{11}" class="weui_search_input" id="search_input" placeholder="搜索" required/>
+                    <input type="number" name="keyword" pattern="[0-9]{11}" class="weui_search_input" id="search_input" placeholder="搜索" required/>
                     <a href="javascript:" class="weui_icon_clear" id="search_clear"></a>
                 </div>
                 <label for="search_input" class="weui_search_text" id="search_text">
@@ -20,12 +22,49 @@
             </form>
             <a href="javascript:" class="weui_search_cancel" id="search_cancel">取消</a>
         </div>
-        <div class="weui_cells weui_cells_access search_show" id="search_show">
-            <div class="weui_cell">
-                <div class="weui_cell_bd weui_cell_primary">
-                    <p>实时搜索文本</p>
-                </div>
+
+        <form action="<?=U('/index/sign')?>" class="ajax-form" method="post" success="sign_success">
+            <div class="weui_cells weui_cells_radio hide" style="margin-top: 0;" id="search_show">
+
             </div>
+
+            <div class="bd hide" id="selected-wrap">
+                <div class="weui_cells">
+                    <div class="weui_cell">
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>手机号码</p>
+                        </div>
+                        <div class="weui_cell_ft" id="mobile"></div>
+                    </div>
+                    <div class="weui_cell">
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>董事长姓名</p>
+                        </div>
+                        <div class="weui_cell_ft" id="chairman_name"></div>
+                    </div>
+                    <div class="weui_cell">
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>性别</p>
+                        </div>
+                        <div class="weui_cell_ft" id="sex">男</div>
+                    </div>
+                    <div class="weui_cell">
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>公司</p>
+                        </div>
+                        <div class="weui_cell_ft" id="company_name"></div>
+                    </div>
+                    <div class="weui_cell">
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>报名时间</p>
+                        </div>
+                        <div class="weui_cell_ft" id="enroll_time"></div>
+                    </div>
+                </div>
+                <div class="weui_btn_area">
+                    <button class="weui_btn weui_btn_primary" type="submit">确定</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
@@ -50,7 +89,7 @@ $(function(){
         }
     };
 
-    var contriner = $('#js-container');
+    var contriner = $(document);
     for (var t in events) {
         for (var type in events[t]) {
             contriner.on(type, t, events[t][type]);
@@ -59,9 +98,43 @@ $(function(){
 
 });
 
+var result_wrap = $("#search_show");
 function search_success(form,resp){
-    alert('aaa');
-    alert(resp.status);
+    result_wrap.html('');
+    $('#selected-wrap').hide();
+    if(resp.list.length  == 0){
+        layer.alert('没有找到匹配的数据！');
+    }
+    $.each(resp.list,function(i,o){
+        $('<label class="weui_cell weui_check_label" for="enroll_id'+o.id+'">' +
+            '<div class="weui_cell_bd weui_cell_primary">' +
+                '<p>'+o.mobile+ '-'+ o.chairman_name+'</p>' +
+            '</div>' +
+            '<div class="weui_cell_ft">' +
+                '<input type="radio" class="weui_check" name="enroll_id" value="'+ o.id+'" id="enroll_id'+o.id+'">' +
+                '<span class="weui_icon_checked"></span>' +
+            '</div>' +
+        '</label>').data('result',o).appendTo(result_wrap);
+    });
+    result_wrap.show();
+    if(resp.list.length == 1){
+        $('.weui_check_label').trigger('click');
+    }
+}
+
+result_wrap.on('click','.weui_check_label',function(){
+    var item = $(this).data('result');
+    $('#mobile').text(item.mobile);
+    $('#chairman_name').text(item.chairman_name);
+    $('#sex').text(item.sex == 'MAN' ? '男' : '女');
+    $('#company_name').text(item.company_name);
+    $('#enroll_time').text(item.create_time);
+    $('#selected-wrap').show();
+});
+
+function sign_success(){
+    result_wrap.html('');
+    $('#selected-wrap').hide();
 }
 </script>
 </block>
