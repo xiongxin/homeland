804f1a9b3c1194f05eede1701a11b743e64ae86e
root <xuebingwang>
2016-04-01 17:23:42 +0800

    签到增加会议的判断

--- src/web/application/controllers/Index.php
+++ src/web/application/controllers/Index.php
@@ -13,6 +13,12 @@ class IndexController extends Mall {
      */
     public function indexAction(){
 //        die('aaa');
+        $query = M()->query("select id,title,address from t_meeting where date(agenda_date) = '".date('Y-m-d')."'");
+        $list = $query ? $query->fetchAll(PDO::FETCH_ASSOC) : false;       
+        if(empty($list)){
+            $this->error('啊哈，今天没有任何会议哦！');
+        }
+        $meeting = $list[0];
     }
 
     public function signAction(){
@@ -26,6 +32,12 @@ class IndexController extends Mall {
         if($this->user['group_id'] != 1){
             $this->error('本功能仅允许运营人员使用！');
         }
+        $query = M()->query("select id,title,address from t_meeting where date(agenda_date) = '".date('Y-m-d')."'");
+        $list = $query ? $query->fetchAll(PDO::FETCH_ASSOC) : false;
+        if(empty($list)){
+            $this->error('啊哈，今天没有任何会议哦！');
+        }
+        $meeting = $list[0];
 
         if(IS_POST){
             $enroll_id = intval(I('enroll_id'));
@@ -33,7 +45,7 @@ class IndexController extends Mall {
             if(empty(I('enroll_id'))){
                 $this->error('参数错误，报名ID不能为空！');
             }
-            if(M('t_enroll')->update(['is_sign'=>Model::BOOL_YES,'sign_time'=>time_format()],['AND'=>['id'=>$enroll_id,'is_affirm'=>Model::BOOL_YES]])){
+            if(M('t_enroll')->update(['is_sign'=>Model::BOOL_YES,'sign_time'=>time_format()],['AND'=>['id'=>$enroll_id,'meeting_id'=>$meeting['id'],'is_affirm'=>Model::BOOL_YES]])){
                 $this->success('签到成功！');
             }else{
                 $this->error('签到失败，请重新再试或联系管理人员！');
@@ -57,7 +69,7 @@ class IndexController extends Mall {
                     'b.company_name',
                 ],
                 [
-                        'a.id'=>$id,
+                        'AND'=>['a.id'=>$id,'meeting_id'=>$meeting['id']]
                 ]);
 
             if(empty($item)){
@@ -85,6 +97,8 @@ class IndexController extends Mall {
             $js_sign = $this->wechat->getJsSign($url);
             $this->assign('js_sign',$js_sign);
         }
+
+        $this->assign('meeting',$meeting);
     }
 
     public function searchAction(){
@@ -93,6 +107,7 @@ class IndexController extends Mall {
         if(empty($keyword)){
             $this->error('请输入手机号码！');
         }
+        $meeting_id = intval(I('meeting_id'));
 
         $list = M('t_enroll(a)')->select(
             [
@@ -108,12 +123,12 @@ class IndexController extends Mall {
             ],
             [
                 'AND'=>[
+                    'a.meeting_id'=>$meeting_id,
                     'a.is_affirm'=>Model::BOOL_YES,
                     'a.is_sign'=>Model::BOOL_NO,
                     'a.mobile[~]'=>$keyword,
                 ]
             ]);
-
 //        SeasLog::info(M()->last_query());
         $this->success('','',['list'=>$list]);
     }
